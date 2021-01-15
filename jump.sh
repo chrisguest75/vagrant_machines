33ae04fb988f09dea9c9b6bb3a17fd1e31cfba13
Chris Guest <chris.guest75@hotmail.com>
2021-01-15 23:21:42 +0000

    Add a new jump script

--- jump.sh
+++ jump.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
-export VAGRANT_MACHINE_VALUES=../../configurations/1gb_docker_ubuntu.yaml
-pushd deployment_types/single_machine
+set -euf -o pipefail
+
+title="Files"
+curdir=$(readlink -f ./configurations)
+curdir=./configurations
+dir_list=$(ls -lhp $curdir | awk -F ' ' ' { print $9 " " $5 } ')
+selection=$(whiptail --title "$title" \
+                        --menu "PgUp/PgDn/Arrow Enter Selects File/Folder\nor Tab Key\n$curdir" 20 80 10 \
+                        --cancel-button Cancel \
+                        --ok-button Select ../ BACK $dir_list 3>&1 1>&2 2>&3)
+
+echo "Selected '$selection' and exitcode '$?'"
+
+# add ../../ as we're going two directories deeper
+export VAGRANT_MACHINE_VALUES="../../$curdir/$selection"
+pushd deployment_types/single_machine 
+echo "************************************************"
+echo "Entering $SHELL to run 'vagrant up --provision'"
+echo "VAGRANT_MACHINE_VALUES=$VAGRANT_MACHINE_VALUES"
+echo "************************************************"
+$SHELL
+popd
